893ce6351976c815bd6665bc2e41d5da98f5b8ae
Hulkingpluto <2486072@students.wits.ac.za>
2024-09-05 14:19:57 +0200

    fixed directory

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'views', 'index.html'));
 });
 
 
